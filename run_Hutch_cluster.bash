745bf04c3639acc63c9d496671c04eaccaee1e06
jbloom <jbloom@fredhutch.org>
2025-04-04 11:11:31 -0700

    rename and update bash run script

---
+++ run_Hutch_cluster.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#SBATCH -c 16
+
+snakemake \
+    -j 16 \
+    -s dms-vep-pipeline-3/Snakefile \
+    --software-deployment-method conda \
+    --rerun-incomplete \
+    --keep-going
